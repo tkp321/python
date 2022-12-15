f11653d6c3c1653de5975baec68164cee7338671
tkp321 <knowledgeable432@gmail.com>
2022-12-15 14:07:59 +0530

    convert feet into inches,yards,miles

---
+++ convert feet into yards,inches,miles.py
@@ -0,0 +1,8 @@
+#convert feet into yards,inches & miles
+x=float(input("Enter your height in feets:"))
+inches=12
+yards=0.333333
+miles=0.000189394
+print("inches of a person:",x*inches)
+print("yards of a person:",x*yards)
+print("miles of a person",x*miles)
