7a6dcdfbf7e9bb450cb7bb9c4daaeb8356ff8cb3
tkp321 <knowledgeable432@gmail.com>
2022-12-16 13:02:27 +0530

    calculating total amount

---
+++ calculating toatal interest.py
@@ -0,0 +1,7 @@
+#calculating total interest and amount of person
+amount=10000
+interest=3.5
+years=7
+totalinterest=amount*interest/100
+actualinterestamount=totalinterest*years
+print(actualinterestamount+amount)
