06c0ad1516972343a3c73ba0988b466c1d4260ef
tkp321 <knowledgeable432@gmail.com>
2022-12-16 12:46:59 +0530

    printing positive,negative numbers

---
+++ printing postive,negative numbers.py
@@ -0,0 +1,10 @@
+#pring positive numbers,negative&zero
+n=float(input("enter your number:"))
+if n>=1:
+    print("positive number")
+elif n<0:
+    print("negative numbers")
+elif n==0:
+    print("zero")
+
+
